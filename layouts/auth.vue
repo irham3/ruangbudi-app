289efb8bb7a078f9557190defe12cb9b052fc808
Irham Tri Ahmadi <irhamtria@gmail.com>
2023-12-26 07:59:57 +0700

    Update auth.vue

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -8,17 +8,17 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="flex min-h-screen flex-col justify-center items-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-lg">
       <!-- <img class="mx-auto h-10 w-auto" src="~/assets/logo.svg" alt="Your Company" /> -->
-      <NuxtLink to="/">
-        Kembali ke beranda
+    </div>
+    <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-lg bg-white shadow-lg rounded px-8 pt-6 pb-8">
+      <NuxtLink to="/" class="text-center transition-transform hover:scale-110">
+        <img src="/images/logo.png" alt="Logo Ruang Budi">
       </NuxtLink>
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 mb-10">
         {{ title }}
       </h2>
-    </div>
-    <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-lg">
       <slot />
     </div>
   </div>
