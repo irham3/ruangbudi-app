43595be834ed0b53df3b109337ce2c8db9842981
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2024-01-02 21:38:08 +0700

    style: update detail-budaya-daerah style

--- pages/detail-budaya-daerah.vue
+++ pages/detail-budaya-daerah.vue
@@ -23,45 +23,20 @@ const navItem = [
   </NuxtLayout> -->
 
   <div class="">
-    <header class="flex items-center justify-between px-[4rem] py-[1rem]">
-      <div class="">
-        <img src="/images/logo.png" alt="">
-      </div>
-      <nav class="flex items-center gap-9">
-        <a href="#" v-for="nav in navItem" :key="nav">{{ nav }}</a>
-      </nav>
-      <button>Masuk</button>
-    </header>
-    <div class="px-32 flex flex-col items-center gap-10 py-16 relative">
-      <div class="absolute w-[20rem] h-[40rem] bg-contain bg-no-repeat top-[-5rem] left-0" style="background-image: url('/images/background/batik.png');"></div>
-      <div class="absolute w-[20rem] h-[40rem] bg-contain bg-no-repeat top-[-5rem] right-0 bg-right" style="background-image: url('/images/background/batik-2.png');"></div>
-      <div class="absolute w-[20rem] h-[20rem] bg-contain bg-no-repeat bottom-0 left-0" style="background-image: url('/images/background/komodo.png');"></div>
+    <NuxtLayout>
+      <div class="px-32 flex flex-col items-center gap-10 py-16 relative">
+        <div class="absolute w-[20rem] h-[40rem] bg-contain bg-no-repeat top-[-5rem] left-0"
+          style="background-image: url('/images/background/batik.png');"></div>
+        <div class="absolute w-[20rem] h-[40rem] bg-contain bg-no-repeat top-[-5rem] right-0 bg-right"
+          style="background-image: url('/images/background/batik-2.png');"></div>
+        <div class="absolute w-[20rem] h-[20rem] bg-contain bg-no-repeat bottom-0 left-0"
+          style="background-image: url('/images/background/komodo.png');"></div>
 
-      <h1 class="text-5xl font-semibold">Keragaman Budaya di Pulau Jawa</h1>
-      <div class="w-[46rem] h-[30rem] bg-contain bg-center bg-no-repeat" style="background-image: url('/images/budaya_jawa-barat.png');"></div>
-      <div class="w-[46rem] h-[30rem] bg-contain bg-center bg-no-repeat" style="background-image: url('/images/budaya_jawa-timur.png');"></div>
+        <h1 class="text-5xl font-semibold">Keragaman Budaya di Pulau Jawa</h1>
+        <div class="w-[46rem] h-[30rem] bg-contain bg-center bg-no-repeat transition-transform hover:scale-105"
+          style="background-image: url('/images/budaya_jawa-barat.png');"></div>
+        <div class="w-[46rem] h-[30rem] bg-contain bg-center bg-no-repeat transition-transform hover:scale-105"
+          style="background-image: url('/images/budaya_jawa-timur.png');"></div>
     </div>
-
-    <footer class="px-36 flex flex-col gap-52 relative py-16">
-      <div class="absolute w-[230px] h-[450px] left-0 top-16"
-        style="background-image: url('/images/background/wayang-bg-2.png');"></div>
-      <div class="absolute w-[230px] h-[450px] right-0 top-16"
-        style="background-image: url('/images/background/wayang-bg.png');"></div>
-
-      <div class="flex justify-between">
-        <div class="flex flex-col gap-1" v-for="i in 5">
-          <h4 class="font-semibold">Belajar Budaya</h4>
-          <a href="" class="text-stone-400">Jawa Barat</a>
-          <a href="" class="text-stone-400">Jawa Barat</a>
-          <a href="" class="text-stone-400">Jawa Barat</a>
-        </div>
-      </div>
-      <div class="flex flex-col items-center">
-        <div class="">
-          <img src="/images/logo.png" alt="">
-        </div>
-        <p>Innovillage 2023 - Ruang Budi - Universitas Telkom</p>
-      </div>
-    </footer>
-  </div>
-</template>
+  </NuxtLayout>
+</div></template>
