f09b2c41f0db75a71ded91611233801f9507cc46
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2024-01-04 06:44:11 +0700

    style: make the /learning-culture page responsive

--- pages/belajar-budaya/index.vue
+++ pages/belajar-budaya/index.vue
@@ -48,32 +48,32 @@ onMounted(async () => {
 <template>
   <NuxtLayout>
     <div class="pb-16">
-      <div class="h-[80vh] flex flex-col w-full items-center px-[2rem]">
+      <div class="h-[35vh] sm:h-[80vh] flex flex-col w-full items-center px-[2rem]">
         <div
           class="w-11/12 flex-1 bg-contain bg-center bg-no-repeat transition-transform duration-700 hover:scale-105"
           style="background-image: url('/images/indonesia.png');"
         />
       </div>
-      <div class="px-32">
+      <div class="sm:px-32 px-4">
         <div class="flex justify-center mb-12">
-          <div class="flex gap-6 border border-stone-400 w-fit px-4 py-2.5 rounded-md items-center">
+          <div class="flex gap-6 border border-stone-400 sm:w-fit w-full px-4 py-2.5 rounded-md items-center">
             <div class="flex items-center gap-4">
               <div class="flex gap-3 items-center hover:scale-105 transition-all">
                 <Icon name="pepicons-pop:pinpoint-filled" class=" text-stone-600" />
-                <input id="" type="text" name="" class="placeholder:text-stone-500 border-transparent focus:border-transparent focus:ring-0 focus:outline-none" placeholder="Provinsi">
+                <input id="" type="text" name="" class="placeholder:text-stone-500 sm:text-base text-sm w-full sm:w-fit border-transparent focus:border-transparent focus:ring-0 focus:outline-none" placeholder="Provinsi">
               </div>
               <div class="w-[1px] h-[32px] bg-stone-300" />
               <div class="flex gap-3 items-center hover:scale-105 transition-all">
                 <Icon name="mdi:leaf" class="rotate-180 text-stone-600" />
-                <input id="" type="text" name="" class="placeholder:text-stone-500 border-transparent focus:border-transparent focus:ring-0 focus:outline-none" placeholder="Kota/Kabupaten">
+                <input id="" type="text" name="" class="placeholder:text-stone-500 sm:text-base text-sm w-full sm:w-fit border-transparent focus:border-transparent focus:ring-0 focus:outline-none" placeholder="Kota/Kabupaten">
               </div>
             </div>
-            <button class="px-6 py-1 bg-[#CA855F] rounded-sm text-white font-semibold transition-transform hover:scale-105">
+            <button class="px-6 py-1 bg-[#CA855F] rounded-sm text-white font-semibold sm:text-base text-sm transition-transform hover:scale-105">
               Telusuri
             </button>
           </div>
         </div>
-        <div class="grid grid-cols-2 gap-x-10 gap-y-8 px-8">
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-10 gap-y-8 px-8">
           <NuxtLink
             v-for="culture in cultures"
             :key="culture.id"
