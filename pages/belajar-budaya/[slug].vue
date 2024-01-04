58f0e63f042b634ac178e22af0ea18e26eebd0f2
Irham Tri Ahmadi <irhamtria@gmail.com>
2024-01-04 18:20:18 +0700

    adding corousel - need improvement

--- pages/belajar-budaya/[slug].vue
+++ pages/belajar-budaya/[slug].vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 import Image from 'primevue/image'
 
-const route = useRoute()
+// const route = useRoute()
 
-const slug = route.params.slug
+// const slug = route.params.slug
 
 definePageMeta({
   title: 'Belajar Budaya',
@@ -19,6 +19,7 @@ definePageMeta({
   </NuxtLayout> -->
 
   <div class="px-32 flex flex-col items-center gap-10 py-16 relative">
+    <MyCarousel />
     <div class="absolute w-[20rem] h-[40rem] bg-contain bg-no-repeat top-[-5rem] left-0" style="background-image: url('/images/background/batik.png');" />
     <div class="absolute w-[20rem] h-[40rem] bg-contain bg-no-repeat top-[-5rem] right-0 bg-right" style="background-image: url('/images/background/batik-2.png');" />
     <div class="absolute w-[20rem] h-[20rem] bg-contain bg-no-repeat bottom-0 left-0" style="background-image: url('/images/background/komodo.png');" />
@@ -26,13 +27,12 @@ definePageMeta({
     <h1 class="text-5xl font-semibold">
       {{ $route.params.slug }}
     </h1>
-    <MyCarousel />
 
     <div class="card flex justify-content-center">
       <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="250" preview />
     </div>
 
     <!-- <div class="w-[46rem] h-[30rem] bg-contain bg-center bg-no-repeat" style="background-image: url('/images/budaya_jawa-barat.png');" />
-    <div class="w-[46rem] h-[30rem] bg-contain bg-center bg-no-repeat" style="background-image: url('/images/budaya_jawa-timur.png');" /> -->
+      <div class="w-[46rem] h-[30rem] bg-contain bg-center bg-no-repeat" style="background-image: url('/images/budaya_jawa-timur.png');" /> -->
   </div>
 </template>
