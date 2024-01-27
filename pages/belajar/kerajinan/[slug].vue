05216a85f928f8696ca32dbeca7d9de470728788
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2024-01-27 20:50:08 +0700

    Refactor styling in [slug].vue

--- pages/belajar/kerajinan/[slug].vue
+++ pages/belajar/kerajinan/[slug].vue
@@ -28,33 +28,33 @@ onMounted(async () => {
 
 <template>
   <NuxtLayout>
-    <div class="px-5 lg:px-32 flex flex-col items-center gap-10 py-16 relative">
+    <div class="px-5 lg:px-32 flex flex-col w-[100dvw] items-center gap-10 py-6 lg:py-16 relative">
       <p class="text-4xl text-center lg:text-start font-bold text-amber-800">
         {{ craft?.title }}
       </p>
 
-      <figure class="w-full h-[32rem] image-full object-cover">
+      <figure class="w-full aspect-video image-full object-cover">
         <iframe
-          class="w-full h-[32rem] rounded-md" :src="`https://www.youtube.com/embed/${craft?.youtube_id}`"
+          class="w-full aspect-video rounded-md" :src="`https://www.youtube.com/embed/${craft?.youtube_id}`"
           :title="craft?.title" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowfullscreen
         />
       </figure>
-      <div class="flex px-36 gap-12 items-center">
+      <div class="flex px-5 lg:px-36 gap-12 w-[100dvw] items-center">
         <div class="w-full lg:w-fit">
-          <div class="group transition ease-in-out hover:translate-y-[-4px]">
+          <div class="group w-full transition ease-in-out hover:translate-y-[-4px]">
             <div class="flex items-center justify-between translate-y-5 px-6">
               <h2 class="text-3xl font-bold bg-white px-2">
                 Deskripsi
               </h2>
             </div>
             <div
-              class="group-hover:border-slate-400 group-hover:shadow-xl border px-6 py-2 text-justify flex gap-2 flex-col pb-3 rounded-md pt-7 relative overflow-hidden z-[-2]"
+              class="group-hover:border-slate-400 w-full group-hover:shadow-xl border px-6 py-2 text-justify flex gap-2 flex-col pb-3 rounded-md pt-7 relative overflow-hidden z-[-2]"
             >
-              <pre class="z-[1] font-sans">{{ craft?.description }}</pre>
+              <pre class="z-[1] font-sans w-full text-wrap">{{ craft?.description }}</pre>
               <div
-                class="absolute bottom-[-13rem] rotate-[-14deg] right-[-5rem] lg:w-[40rem] translate-x-[7rem] translate-y-[3rem] group-hover:translate-x-0 group-hover:translate-y-0 transition-transform ease-in-out duration-1000 h-56 bg-gradient-to-t from-orange-300 to-transparent z-[-1]"
+                class="absolute bottom-[-13rem] rotate-[-14deg] right-[-5rem] w-full lg:w-[40rem] translate-x-[7rem] translate-y-[3rem] group-hover:translate-x-0 group-hover:translate-y-0 transition-transform ease-in-out duration-1000 h-56 bg-gradient-to-t from-orange-300 to-transparent z-[-1]"
               />
             </div>
           </div>
