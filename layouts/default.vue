e3767be840d73c486606d3a4b8d4ba689e195093
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2024-01-31 02:58:45 +0700

    create transition effect to layout default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
   <Header />
-  <div class="flex flex-col">
+  <div class="flex flex-col transition-opacity duration-500" :style="{opacity: loading?'0%': '100%'}">
     <slot />
   </div>
   <Footer />
 </template>
+
+<script setup lang="ts">
+const loading = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false
+  }, 300)
+})
+</script>
